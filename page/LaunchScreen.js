67b5f87806ffe230b981dd6b00334c4687f07141
TienSinh03 <zatos1232@gmail.com>
2024-10-17 16:12:02 +0700

    add LaunchScreen

---
+++ page/LaunchScreen.js
@@ -0,0 +1,79 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ImageBackground,
+  Image,
+  TouchableOpacity
+} from "react-native";
+
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+    {/* Background Image */}
+      <ImageBackground source={require('../assets/image/Launch Screen/Image 30.png')} resizeMode="cover" style={styles.imageBackground}>
+       {/* Icon Header */}
+        <Image source={require('../assets/image/Launch Screen/Image 33.png')} style ={styles.imageIcon}/>
+
+        {/* Title header */}
+        <Text style={styles.titleHeader}>Your music {'\n'} Your{'\n'}artists</Text>
+
+        {/* Button Create an account */}
+        <TouchableOpacity style={[styles.button, styles.buttonCreateAc]}>
+            <Text style={[styles.textButton,{ color: 'white'}]}>Create an account</Text>
+        </TouchableOpacity>
+
+        {/* Button Already have an account */}
+        <TouchableOpacity style={[styles.button, styles.buttonAlready]}>
+            <Text style={[styles.textButton,{ color: '#9d45efff'}]}>l already have an account</Text>
+        </TouchableOpacity>
+
+      </ImageBackground>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageBackground: {
+    flex: 1,
+    alignItems: "center",
+  },
+  imageIcon:{
+    marginTop: 60,
+  },
+  titleHeader:{
+    marginTop: '70%',
+    textAlign:'center',
+    fontSize:40,
+    lineHeight: 56,
+    fontWeight: 'bold', 
+    color: 'white'
+  },
+  button: { 
+    alignItems: "center",
+    paddingVertical: 15,
+    width:'90%',
+    borderRadius:26
+  },
+  buttonCreateAc:{
+    marginTop: '25%',
+    backgroundColor: "#171A1FFFF",
+  },
+  buttonAlready:{
+    marginTop: 20,
+    backgroundColor: "#F7F1FEFF",
+  },
+  textButton:{
+    fontSize:18,
+    fontWeight:'400',
+  }
+
+
+
+  
+});
