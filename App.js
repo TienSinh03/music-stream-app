84028c4a7fe69e109d37a5a70e680be9cbbbe618
TienSinh03 <zatos1232@gmail.com>
2024-10-27 17:07:24 +0700

    add Screen Home Audio

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import LanchScreen from './page/LaunchScreen';
 import LanchScreen_Premium from './page/LaunchScreen_Premium';
+import Home_AudioListing from './page/Home_AudioListing';
 
 export default function App() {
   return (
     // <LanchScreen/>
-    <LanchScreen_Premium/>
+    // <LanchScreen_Premium/>
+    <Home_AudioListing/>
   );
 }
---
+++ page/Home_AudioListing.js
@@ -0,0 +1,302 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  ScrollView,
+  StatusBar,
+  ImageBackground,
+  Dimensions
+} from "react-native";
+import React, { useEffect, useState } from "react";
+
+import IconFe from "react-native-vector-icons/Feather";
+import IconAnt from "react-native-vector-icons/AntDesign";
+import IconIon from "react-native-vector-icons/Ionicons";
+
+
+const screenWith = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={{marginLeft: 20}}>
+        {/* Header */}
+        <View style={styles.header}>
+          {/* Image lyrics */}
+          <Image
+            source={require("../assets/image/Home - Audio Listing/Image 36.png")}
+          />
+
+          <View style={styles.headerLeft}>
+            <IconFe
+              name="bell"
+              size={30}
+              color="black"
+              style={{ marginRight: 15 }}
+            />
+            <TouchableOpacity>
+              <Image
+                source={require("../assets/image/Home - Audio Listing/Avatar 3.png")}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* Information user login and Search */}
+        <View style={{ display: "flex", marginVertical: 40,marginRight:20 }}>
+          {/* Text Good morning */}
+          <Text style={styles.textMorning}>Good morning</Text>
+          {/* Text User */}
+          <Text style={styles.textUser}>Ashley Scott</Text>
+
+          {/* Input search */}
+          <View style={styles.inputSearch}>
+            <IconFe name="search" size={20} />
+            <TextInput
+              placeholder="What you want to listen to"
+              style={styles.textInputSearch}
+            />
+          </View>
+        </View>
+
+        {/* Suggestion */}
+        <View style ={{marginBottom:30}}>
+          {/* Titile */}
+          <Text style={[styles.textTitleCategory,{marginBottom:10}]}>Suggestion for you</Text>
+
+          {/* List suggestion */}
+          <View style ={{display:"flex", flexDirection:'row', alignItems:'center'}} >
+              
+              <ImageBackground source={require('../assets/image/Home - Audio Listing/image_sugges_1.jpg')} style={[styles.imageSugges,{marginRight:60}]}>
+                  <View style={styles.textNameSugges}>
+                    <Text style={{color:'white', fontSize:20, fontWeight:'500'}}>Reflection</Text>
+                    <Text style={{color:'white', fontSize:16, fontWeight:'500'}}>Christina Aguilera</Text>
+                  </View>
+              </ImageBackground>
+
+              <ImageBackground source={require('../assets/image/Home - Audio Listing/image_sugges_2.jpg')} style={[styles.imageSugges]}>
+                  <View style={styles.textNameSugges}>
+                    <Text style={{color:'white', fontSize:20, fontWeight:'500'}}>In The Stars</Text>
+                    <Text style={{color:'white', fontSize:16, fontWeight:'500'}}>Benson Boone</Text>
+                  </View>
+              </ImageBackground>
+          </View>
+        </View>
+
+        {/* Charts */}
+        <View style ={{marginBottom:30}}>
+
+          {/** Name categories chart and button see all */}
+          <View style ={styles.viewTitileCate}>
+            <Text style={styles.textTitleCategory}>Charts</Text>
+            <TouchableOpacity style ={{alignItems:'center'}}>
+              <Text style={styles.text}>See all</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/** List chart */}
+          <View style ={{display:"flex", flexDirection:'row', alignItems:'center',gap:20}} >
+
+              {/** Top 50 Canada */}
+              <TouchableOpacity style={{width:'33%'}}>
+                <Image source={require('../assets/image/Home - Audio Listing/Container 31.png')} style={{}}/>
+                <Text style={[styles.textDes]}>Daily chart-toppers update</Text>
+              </TouchableOpacity>
+
+              {/** Top 50 Global */}
+              <TouchableOpacity style={{width:'33%'}}>
+                <Image source={require('../assets/image/Home - Audio Listing/Container 32.png')} style={{}}/>
+                <Text style={[styles.textDes]}>Daily chart-toppers update</Text>
+              </TouchableOpacity>
+
+              {/** Top 50 Trending */}
+              <TouchableOpacity style={{width:'33%'}}>
+                <Image source={require('../assets/image/Home - Audio Listing/Container 33.png')} style={{}}/>
+                <Text style={[styles.textDes]}>Daily chart-toppers update</Text>
+              </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* Trending albums */}
+        <View style ={{marginBottom:30}}>
+          {/** Name categories trending albums and button see all */}
+          <View style ={styles.viewTitileCate}>
+            <Text style={[styles.textTitleCategory]}>Trending albums</Text>
+            <TouchableOpacity style ={{alignItems:'center'}}>
+              <Text style={styles.text}>See all</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/** List trending albums */}
+          <View style ={{display:"flex", flexDirection:'row', alignItems:'center',gap:20}} >
+            
+              <TouchableOpacity style={{width:'33%'}}>
+                <Image source={require('../assets/image/Home - Audio Listing/Image 45.png')} style={{}}/>
+                <Text style={[styles.textNameMainCate,{marginTop:5}]}>ME</Text>
+                <Text style={styles.nameArists}>Jessica Gonzalez</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{width:'33%'}}>
+                <Image source={require('../assets/image/Home - Audio Listing/Image 46.png')} style={{}}/>
+                <Text style={[styles.textNameMainCate,{marginTop:5}]}>Magna nost</Text>
+                <Text style={styles.nameArists}>Brian Thomas</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{width:'33%'}}>
+                <Image source={require('../assets/image/Home - Audio Listing/Image 47.png')} style={{}}/>
+                <Text style={[styles.textNameMainCate,{marginTop:5}]}>Magna nost</Text>
+                <Text style={styles.nameArists}>Christopher Brown</Text>
+              </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* Popular artists */}
+        <View style ={{marginBottom:30}}>
+
+          {/** Name categories popular artists and button see all */}
+          <View style ={styles.viewTitileCate}>
+            <Text style={[styles.textTitleCategory]}>Popular artists</Text>
+            <TouchableOpacity style ={{alignItems:'center'}}>
+              <Text style={styles.text}>See all</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/** List popular artists */}
+          <View style ={{display:"flex", flexDirection:'row', alignItems:'center',gap:20}} >
+              
+              <View style={{width:'33%', alignItems:'center'}}>
+                <Image source={require('../assets/image/Home - Audio Listing/Image 39.png')} style={{}}/>
+                
+                <Text style={[styles.textNameMainCate,{marginVertical:8}]}>Jennifer Wilson</Text>
+                
+                {/** button follow */}
+                <TouchableOpacity style ={styles.buttonFL}>
+                  <Text style={styles.textButtonFL}>Follow</Text>
+                </TouchableOpacity>
+              </View>
+
+              
+              <View style={{width:'33%', alignItems:'center'}}>
+                <Image source={require('../assets/image/Home - Audio Listing/Image 40.png')} style={{}}/>
+                
+                <Text style={[styles.textNameMainCate,{marginVertical:8}]}>Elizabeth Hall</Text>
+                
+                {/** button follow */}
+                <TouchableOpacity style ={styles.buttonFL}>
+                  <Text style={styles.textButtonFL}>Follow</Text>
+                </TouchableOpacity>
+              </View>
+
+              
+              <View style={{width:'33%', alignItems:'center'}}>
+                <Image source={require('../assets/image/Home - Audio Listing/Image 41.png')} style={{}}/>
+                
+                <Text style={[styles.textNameMainCate,{marginVertical:8}]}>Anthony Taylor</Text>
+                
+                {/** button follow */}
+                <TouchableOpacity style ={styles.buttonFL}>
+                  <Text style={styles.textButtonFL}>Follow</Text>
+                </TouchableOpacity>
+              </View>
+          </View>
+        </View>
+      </ScrollView>
+
+      {/** action footer */}
+      <View style={{display:'flex', flexDirection:'row', justifyContent:'space-between', alignItems:'center',paddingHorizontal:55,paddingVertical:20,backgroundColor:'white', borderTopWidth:1, borderColor:'#C4C4C4'}}>
+        {/** button home */}
+        <TouchableOpacity style={{alignItems:'center'}}>
+          <IconAnt name="home" size={25} color="#21c5db"/>
+          <Text style={{fontSize:14, lineHeight:24, fontWeight:'400', color:'#21c5db'}}>Home</Text>
+        </TouchableOpacity>
+
+        {/** button search */}
+        <TouchableOpacity style={{alignItems:'center'}}>
+          <IconFe name="search" size={25} color="#565E6CFF"/>
+          <Text style={{fontSize:14, lineHeight:24, fontWeight:'400', color:'#565E6CFF'}}>Search</Text>
+        </TouchableOpacity> 
+
+        {/** button feed */}
+        <TouchableOpacity style={{alignItems:'center'}}>
+          <IconAnt name="switcher" size={25} color="#565E6CFF"/>
+          <Text style={{fontSize:14, lineHeight:24, fontWeight:'400', color:'#565E6CFF'}}>Feed</Text>
+        </TouchableOpacity>
+
+        {/** button library */}
+        <TouchableOpacity style={{alignItems:'center'}}>
+          <IconIon name="library-outline" size={25} color="#565E6CFF"/>
+          <Text style={{fontSize:14, lineHeight:24, fontWeight:'400', color:'#565E6CFF'}}>Library</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    marginRight:20
+  },
+  headerLeft: { display: "flex", alignItems: "center", flexDirection: "row" },
+  textMorning: { fontSize: 16, lineHeight: 24, color: "#565E6CFF" },
+  textUser: {
+    fontSize: 24,
+    lineHeight: 36,
+    fontWeight: "bold",
+    paddingTop: 5,
+    color: "#171A1F",
+  },
+  inputSearch: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#BCC1CAFF",
+    width: "100%",
+    padding: 10,
+    borderRadius: 22,
+    marginTop: 11,
+  },
+  textInputSearch: {
+    fontSize: 16,
+    color: "#C4C4C4",
+    fontWeight: "400",
+    marginLeft: 10,
+  },
+  imageSugges:{
+    flex:1,width: screenWith*0.5, height:screenHeight*0.301, justifyContent:"flex-end"
+  },
+  textTitleCategory: {
+    fontSize: 24,
+    lineHeight: 30,
+    fontWeight: "bold",
+    color: "#171A1F",
+  },
+  viewTitileCate:{
+    display:"flex", flexDirection:'row', alignItems:'center', justifyContent:'space-between', marginRight:20,marginBottom:10
+  },
+  text:{fontSize:16, lineHeight:24, fontWeight:'400', color:'#9095A0FF'},
+  textDes:{
+    fontSize:16, lineHeight:24, fontWeight:'400', color:'#9095A0FF',marginTop:5
+  },
+  textNameSugges:{display:'flex', padding:20, width:screenWith*0.5, height:screenHeight*0.085,backgroundColor:'#171A1F80'},
+  textNameMainCate:{
+    fontSize:16, lineHeight:24, fontWeight:'500', color:'#171A1FFF'
+  },
+  nameArists:{fontSize:16, lineHeight:22, fontWeight:'400', color:'#9095A0FF'},
+  buttonFL:{alignItems:'center', backgroundColor:'#171A1FFF', borderRadius:18},
+  textButtonFL:{fontSize:16, lineHeight:22, fontWeight:'400', color:'white', paddingHorizontal:12, paddingVertical:8}
+});
